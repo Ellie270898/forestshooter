ebd72baaf5229f322c38cd63b910873ed34a3fa6
ellie270898 <ellieharrodine@gmail.com>
2018-01-01 15:26:41 +0000

    high score and enemy code added

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var game = new Phaser.Game(650, 650, Phaser.CANVAS, 'phaser-example', { preload:
 function preload() {
 
     game.load.image('background','assets/forestmap.png');
-    game.load.image('playerimage','assets/character3.png');
     game.load.spritesheet('moving', 'assets/animation6.png', 108, 140);
+    game.load.image('foreground', 'assets/foreground.png');
+    game.load.image('ship', 'assets/ship.png');
+    game.load.image('enemy', 'assets/enemy.png');
+    game.load.image('bullet', 'assets/bullet.png');
 
 }
 
@@ -16,24 +19,57 @@ function create() {
 
     game.physics.startSystem(Phaser.Physics.P2JS);
 
-    game.add.tileSprite(0, 0, 4094, 650, 'background');
+    game.add.tileSprite(0, 0, 3440, 650, 'background');
 
-    game.world.setBounds(0, 0, 4094, 650);
+    game.world.setBounds(0, 0, 3040, 650);
 
-    player1 = this.moving=game.add.sprite(120, 450, "moving");
+    player1 = this.moving=game.add.sprite(120, 420, "moving");
+      game.physics.enable(player1, Phaser.Physics.ARCADE);
       this.moving.animations.add('still', [8], 1, true);
-      this.moving.animations.add('walk', [0,1,2,3,4,5,6,7], 22, true);
+      this.moving.animations.add('walk', [0,1,2,3,4,5,6,7], 18, true);
       this.speed = 4;
 
-//    player1 = game.add.sprite(0, 0, 'playerimage');
 
-  //  game.physics.p2.enable(player1);
+    this.enemy = game.add.group();
+    this.enemy.enableBody = true;
+    this.enemy.physicsBodyType = Phaser.Physics.ARCADE;
+
+    this.bullets = game.add.group();
+    this.bullets.enableBody = true;
+    this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+    for (let i = 0; i < 20; i++) {
+      let b = this.bullets.create(0, 0, 'bullet');
+      b.exists = false;
+      b.visible = false;
+      b.checkWorldBounds = true;
+      b.events.onOutOfBounds.add((bullet) => { bullet.kill(); });
+    }
+
 
     game.camera.follow(player1);
+
+    //This adds some trees to the front, adding more depth to the game world
+    game.add.sprite(0, 0, 'foreground');
+
+    this.highScore = localStorage.getItem('shooterhighscore');
+    if (this.highScore === null) {
+      localStorage.setItem('shooterhighscore', 0);
+      this.highScore = 0;
+    }
+
+      this.score = 0;
+      var score = game.add.text(500, 20, `Score: ${this.score} \nHighScore: ${this.highScore}`, { font: '20px Arial', fill: '#ffffff' });
+      score.fixedToCamera = true;
+
+    this.cursors = game.input.keyboard.createCursorKeys();
+    game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
 
 
 
 }
+
+
 
 function update() {
 
