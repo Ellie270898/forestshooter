0edfb736bf9c6d5e6ad2fa82cfad45ff7245eaad
ellie270898 <ellieharrodine@gmail.com>
2018-01-11 02:19:41 +0000

    sound added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//var game = new Phaser.Game(650, 650, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
 const mainState =
 {
         preload: function ()
@@ -16,44 +16,40 @@ const mainState =
           game.load.image('gameover', 'assets/gameoverscreen2.png');
           game.load.image('gamewin', 'assets/youwin.png');
           game.load.image('gameStartScreen', 'assets/gameStartScreen.png');
+          game.load.audio('gamesoundtrack', 'assets/gamesoundtrack1.mp3');
+          game.load.audio('fire', 'assets/shootsound.mp3');
       },
-
-
-    //var spritesheet;
 
          create: function ()
         {
 
           var player1;
           var cursors;
-
-
+          //THE MAP
           this.game.physics.startSystem(Phaser.Physics.ARCADE);
-
-
           game.add.tileSprite(0, 0, 3440, 650, 'background');
-
-          this.gameStartScreen = game.add.sprite(0, 0, 'gameStartScreen');
-
           game.world.setBounds(0, 0, 3040, 650);
-
+          //THE STARTSCREEN
+          this.gameStartScreen = game.add.sprite(0, 0, 'gameStartScreen');
+          //THE MUSIC
+          this.soundtrack = game.add.audio('gamesoundtrack', 0.8);
+          this.fireSound = game.add.audio('fire', 0.1);
+          //START FUNCTION
           game.input.onDown.add(this.beginning, this);
-
+          //CAT ANIMATION
           this.gameStart = this.cat1 = game.add.sprite(73, -40, 'cat');
           this.gameStart.enableBody = true;
           game.physics.arcade.enable(this.gameStart);
           this.gameStart.physicsBodyType = Phaser.Physics.ARCADE;
           this.gameStart.collideWorldBounds = true;
           this.cat1.animations.add('start', [0,1,2,3,4,5], 7);
-
+          //CAT AT THE END
           this.gameEnd = this.cat = game.add.sprite(2700, -30, 'cat');
           this.gameEnd.enableBody = true;
           game.physics.arcade.enable(this.gameEnd);
           this.gameEnd.physicsBodyType = Phaser.Physics.ARCADE;
           this.gameEnd.collideWorldBounds = true;
           this.cat.animations.add('end', [0], 1, true);
-
-
           //This code creates the player and animations for running and walking
           this.player1 = this.spritesheet =game.add.sprite(120, 420, "spritesheet");
           this.player1.enableBody = true;
@@ -64,7 +60,6 @@ const mainState =
           this.spritesheet.animations.add('walk', [0,1,2,3,4,5,6,7], 18, true);
           this.spritesheet.animations.add('shoot', [9], 1, true);
           this.speed = 4;
-
           //FIRST SPIDER
           this.greyspider = this.spider =game.add.sprite(1000, -100, 'spider');
           this.greyspider.enableBody = true;
@@ -72,7 +67,6 @@ const mainState =
           this.greyspider.physicsBodyType = Phaser.Physics.ARCADE;
           this.spider.animations.add('spidermove', [0,1,2,3,4,3,2,1], 4, true);
           this.spider.healthPoints = 100;
-
           //SECOND SPIDER
           this.greyspider2 = this.spider2 =game.add.sprite(1780, -100, 'spider2');
           this.greyspider2.enableBody = true;
@@ -80,7 +74,6 @@ const mainState =
           this.greyspider2.physicsBodyType = Phaser.Physics.ARCADE;
           this.spider2.animations.add('spidermove2', [3,4,3,2,1,0,1,2], 4, true);
           this.spider2.healthPoints = 100;
-
           //THIRD SPIDER
           this.greyspider3 = this.spider3 =game.add.sprite(2580, 0, 'spider3');
           this.greyspider3.enableBody = true;
@@ -88,16 +81,11 @@ const mainState =
           this.greyspider3.physicsBodyType = Phaser.Physics.ARCADE;
           this.spider3.animations.add('spidermove3', [1,2,3,4,3,2,1,0], 4, true);
           this.spider3.healthPoints = 100;
-
-
-
           //BULLETS
           this.bullets = game.add.group();
           this.bullets.enableBody = true;
           this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
           this.bullets.lifespan = 2;
-
-
             for (let i = 0; i < 20; i++) {
               let b = this.bullets.create(0, 0, 'bullet');
               b.exists = false;
@@ -105,17 +93,12 @@ const mainState =
               b.checkWorldBounds = true;
               b.events.onOutOfBounds.add((bullet) => { bullet.kill(); });
         }
-
-
-
           this.bulletTime = 0;
-
         //This tells the camera to follow the player
           game.camera.follow(player1);
-
           //This adds some trees to the front, adding more depth to the game world
             game.add.sprite(0, 0, 'foreground');
-
+          //This counts the high score
           this.highScore = localStorage.getItem('shooterhighscore');
           if (this.highScore === null) {
             localStorage.setItem('shooterhighscore', 0);
@@ -123,19 +106,17 @@ const mainState =
           }
           //This displays a score and determines the font, size and colour
           this.score = 0;
-          this.scoreDisplay = game.add.text(500, 20, `Score: ${this.score} \nHighScore: ${this.highScore}`, { font: '20px Arial', fill: '#ffffff' });
+          this.scoreDisplay = game.add.text(20, 20, `Score: ${this.score} \nHighScore: ${this.highScore}`, { font: '25px Calibra', fill: '#ffffff' });
           this.scoreDisplay.fixedToCamera = true;
 
           this.cursors = game.input.keyboard.createCursorKeys();
           game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
-
-
-
         },
 
+        //shooting mechanism
         fire: function () {
          if (game.time.now > this.bulletTime) {
-          //  this.fireSound.play();
+
 
             let bullet = this.bullets.getFirstExists(false);
             if (bullet) {
@@ -148,64 +129,51 @@ const mainState =
           }
         },
 
-
-
+      //sound and visuals for shooting
       shoot: function(){
         //console.log("animation");7
         //this.spritesheet.x+=this.speed;
         this.spritesheet.play('shoot');
         this.spritesheet.scale.x=1;
       },
-
+      //kills spider when hit
       hit: function (bullet, greyspider) {
           this.score = this.score + 50;
           this.greyspider.kill();
           this.scoreDisplay.text = `Score: ${this.score} \nHighScore: ${this.highScore}`;
-
-
     },
-
+    //kills spider when hit
     hit2: function (bullet, greyspider2) {
         this.score = this.score + 50;
         this.greyspider2.kill();
         this.scoreDisplay.text = `Score: ${this.score} \nHighScore: ${this.highScore}`;
-
-
   },
-
-  hit3: function (bullet, greyspider3) {
+  //kills spider when hit
+    hit3: function (bullet, greyspider3) {
       this.score = this.score + 50;
       this.greyspider3.kill();
       this.scoreDisplay.text = `Score: ${this.score} \nHighScore: ${this.highScore}`;
-
-
-},
-
-  beginning: function() {
-    this.gameStartScreen.kill();
-    this.cat1.animations.play('start');
-  },
-
+    },
+    //START SCREEN
+    beginning: function() {
+      this.gameStartScreen.kill();
+      this.soundtrack.play();
+      this.cat1.animations.play('start');
+      },
+      //KILLS PLAYER WHEN THEY COLLIDE WITH AN ENEMY
       player1GotHit: function () {
         console.log('you died!');
         this.player1.kill();
         game.state.start('gameover');
-
       },
-
+      //PLAYER WINS AND MOVES ON TO NEXT GAME
       player1Win: function () {
         console.log('you win!');
         this.player1.kill();
         game.state.start('gamewin');
 
       },
-
-
-
-
-
-
-
+      //FINISHES BOTH GAMES
       gameover: function () {
         if (this.score > this.highScore) {
           this.highScore = this.score;
@@ -248,6 +216,7 @@ const mainState =
               //console.log("shoot");
               this.shoot();
               this.fire();
+              this.fireSound.play();
             }
             else
           {
@@ -258,17 +227,13 @@ const mainState =
           }
         },
 
-
       render: function () {
 
-        game.debug.cameraInfo(game.camera, 32, 32);
-        game.debug.spriteCoords(this.player1, 32, 500)
-      //  game.debug.text("Time until event: " + game.time.events.duration, 32, 32);
+        //game.debug.cameraInfo(game.camera, 32, 32);
+      //  game.debug.spriteCoords(this.player1, 32, 500);
 
       },
-
-}
-
+    }
   const gameoverState = {
     preload: function () {
       game.load.image('gameover', 'assets/gameoverscreen3.png');
@@ -283,13 +248,14 @@ const mainState =
     }
   };
 
-
+  //LOADS MINI GAME AFTER PLAYER WINS
   const gamewinState = {
     preload: function () {
       game.load.image('gamewin', 'assets/youwin.png');
     },
     create: function () {
       const gameOverImg = game.cache.getImage('gamewin');
+      game.sound.stopAll();
       game.add.sprite(
         game.camera.centerX - gameOverImg.width / 2,
         game.camera.centerY - gameOverImg.height / 2,
@@ -297,11 +263,18 @@ const mainState =
       game.input.onDown.add(() => { game.state.start('mini'); });
     }
   };
+  //CODE FOR MINI GAME
   const miniState = {
 
   create: function () {
     game.add.tileSprite(0, 0, 650, 650, 'background2');
     game.world.setBounds(0, 0, 650, 650);
+
+    this.minigamesoundtrack = game.add.audio('minigamesoundtrack');7
+    this.moveright = game.add.audio('moveright.mp3');
+    this.moveleft = game.add.audio('moveleft.mp3');
+
+    this.minigamesoundtrack.play();
 
     this.ship = game.add.sprite(400, 550, 'ship');
     game.physics.enable(this.ship, Phaser.Physics.ARCADE);
@@ -329,14 +302,6 @@ const mainState =
 
     this.bulletTime = 0;
 
-  //  this.explosion = this.game.add.sprite(0, 0, 'explode');
-  //  this.explosion.exists = false;
-  //  this.explosion.visible = false;
-    // this.explosion.frame = 6; // show one frame of the spritesheet
-  //  this.explosion.anchor.x = 0.5;
-  //  this.explosion.anchor.y = 0.5;
-  //  this.explosion.animations.add('boom');
-
     this.highScore = localStorage.getItem('invadershighscore');
     if (this.highScore === null) {
       localStorage.setItem('invadershighscore', 0);
@@ -346,7 +311,7 @@ const mainState =
     this.score = 0;
     this.scoreDisplay = game.add.text(200, 20, `Score: ${this.score} \nHighScore: ${this.highScore}`, { font: '30px Arial', fill: '#ffffff' });
 
-    this.fireSound = game.add.audio('fire');
+    this.fireSound = game.add.audio('fire', '0.1');
 
     this.cursors = game.input.keyboard.createCursorKeys();
     game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
@@ -369,6 +334,7 @@ const mainState =
       this.highScore = this.score;
       localStorage.setItem('invadershighscore', this.highScore);
     }
+    game.sound.stopAll();
     game.state.start('gameover');
   },
 
@@ -388,18 +354,19 @@ const mainState =
     game.load.image('ship', 'assets/spaceCat.png');
     game.load.image('enemy', 'assets/spaceSpider1.png');
     game.load.image('bullet', 'assets/bullet.png');
-    //game.load.spritesheet('explode', 'assets/explode.png', 128, 128);
-    //game.load.audio('fire', 'assets/fire.mp3');
+    game.load.audio('minigamesoundtrack', 'assets/minigamesoundtrack1.mp3');
+    game.load.audio('moveright', 'assets/moveright.mp3');
+    game.load.audio('move;eft', 'assets/moveleft.mp3');
   },
 
   shipGotHit: function (alien, ship) {
   //  this.explosion.reset(this.ship.x + (this.ship.width / 2), this.ship.y + (this.ship.height / 2));
     this.ship.kill();
+    game.sound.stopAll();
     //this.explosion.animations.play('boom');
   },
 
   update: function () {
-
 
     game.physics.arcade.overlap(this.bullets, this.aliens, this.hit, null, this);
     game.physics.arcade.overlap(this.aliens, this.ship, this.shipGotHit, null, this);
@@ -413,8 +380,10 @@ const mainState =
     );
 
     if (this.cursors.left.isDown) {
+      this.moveleft.play();
       this.ship.body.velocity.x = -300;
     } else if (this.cursors.right.isDown) {
+      this.moveright.play();
       this.ship.body.velocity.x = 300;
     }
     if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
@@ -425,9 +394,10 @@ const mainState =
 
 const gameover1State = {
   preload: function () {
-    game.load.image('gameover', 'assets/gameover.jpg');
+    game.load.image('gameover', 'assets/gameoverscreen3.png');
   },
   create: function () {
+    game.sound.stopAll();
     const gameOverImg = game.cache.getImage('gameover');
     game.add.sprite(
       game.world.centerX - gameOverImg.width / 2,
